2bc6c4f7b7ec10bd251f341a683b7fe07fb93649
Jack Yeung <jyeung.se@gmail.com>
2018-06-06 16:50:23 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,11 +1,19 @@
 var playlist = new Object({artist: "song"});
 
+<<<<<<< HEAD
 function updatePlaylist(playlist, artistName, songTitle) {
+=======
+var updatePlaylist = function(playlist, artistName, songTitle) {
+>>>>>>> 75b8ca6d836e04ad344abefe1c46982b7b7612df
   playlist[artistName] = songTitle;
   return playlist;
 }
 
 function removeFromPlaylist(playlist, artistName) {
+<<<<<<< HEAD
   delete playlist[artistName];
+=======
+  delete playlist.[artistName];
+>>>>>>> 75b8ca6d836e04ad344abefe1c46982b7b7612df
   return playlist;
 }
